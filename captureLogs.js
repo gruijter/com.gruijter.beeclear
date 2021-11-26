db00e9b5849df6b2d1ca7852843f5119dbacc4f1
gruijter <gruijter@hotmail.com>
2021-11-26 12:05:47 +0100

    1.0.2

--- captureLogs.js
+++ captureLogs.js
@@ -1,9 +1,9 @@
 /*
-Copyright 2020, Robin de Gruijter (gruijter@hotmail.com)
+Copyright 2020 - 2021, Robin de Gruijter (gruijter@hotmail.com)
 
-This file is part of com.gruijter.facepp.
+This file is part of com.gruijter.beeclear.
 
-com.gruijter.facepp is free software: you can redistribute it and/or modify
+com.gruijter.beeclear is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
@@ -14,7 +14,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with com.gruijter.facepp.  If not, see <http://www.gnu.org/licenses/>.
+along with com.gruijter.beeclear.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 'use strict';
